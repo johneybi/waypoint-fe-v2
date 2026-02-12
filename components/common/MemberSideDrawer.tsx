2eacf6a95c396ff2d487a656fae73bd1f7a315a9
Yusi <yusi4781@naver.com>
2026-02-13 01:21:18 +0900

    feat(sidedrawer): 멤버 관리하는 side drawer 구현 [WAPO-153]

---
+++ components/common/MemberSideDrawer.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import { ChevronRight, DoorClosed, UserPlus, X } from "lucide-react";
+import { Button } from "../ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "../ui/drawer";
+import BookMarkIcon from "./BookmarkIcon";
+import { useState } from "react";
+import {
+  mockCollectionMembersResponse,
+  mockPlanMembersResponse,
+} from "@/mocks/member";
+import Image from "next/image";
+import Divider from "./Divider";
+
+interface MemberSideDrawerProps {
+  title: string;
+  placeCount?: number;
+  variant: "COLLECTION" | "PLAN";
+}
+
+const MemberSideDrawer: React.FC<MemberSideDrawerProps> = ({
+  title,
+  placeCount,
+  variant,
+}: MemberSideDrawerProps) => {
+  // TODO: 멤버 목록 조회 API 호출
+  const [members, setMembers] = useState(
+    variant === "COLLECTION"
+      ? mockCollectionMembersResponse
+      : mockPlanMembersResponse,
+  );
+
+  return (
+    <Drawer direction="right">
+      <DrawerTrigger asChild>
+        {/* TODO: HeaderButton 구현 시 교체 */}
+        <Button>사이드바 열기</Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerClose className="absolute top-3 right-3">
+          <X size={24} />
+        </DrawerClose>
+        <DrawerHeader className="flex flex-col gap-1 w-full justify-center items-center mt-10">
+          <DrawerTitle className="flex flex-row gap-1">
+            <BookMarkIcon strokeWidth={2} size={28} color="#0ea5e9" />
+            <p className="typography-display-xl">{title}</p>
+          </DrawerTitle>
+          <DrawerDescription className="typography-action-sm-bold font-[#757575]">
+            {placeCount}개의 장소
+          </DrawerDescription>
+        </DrawerHeader>
+        <main className="flex flex-col gap-3 mx-5 mt-10">
+          {/* 여행 멤버 박스 */}
+          <div className="w-full rounded-2xl bg-[#f0f0f0]">
+            <div className="flex px-4 py-3 justify-between">
+              <p className="typography-action-base-bold">여행 멤버</p>
+              <p className="typography-action-sm-reg font-[#757575]">
+                관리하기
+              </p>
+            </div>
+            <div className="px-3 flex flex-col gap-3">
+              {/* 멤버 나열 박스 */}
+              <div className="flex flex-col gap-3">
+                {members.members.map((member) => (
+                  <div
+                    className="p-2 flex flex-row gap-2 items-center"
+                    key={member.nickname}
+                  >
+                    {member.picture && (
+                      <>
+                        <Image
+                          width={28}
+                          height={28}
+                          src={member.picture}
+                          alt={member.nickname ?? ""}
+                          className="rounded-full"
+                        />
+                        <p className="typography-action-sm-reg">
+                          {member.nickname}
+                        </p>
+                      </>
+                    )}
+                  </div>
+                ))}
+              </div>
+              <div className="flex flex-col">
+                <Divider />
+                <Button
+                  icon={<UserPlus size={18} className="opacity-40" />}
+                  variant="ghost"
+                  className="rounded-2xl typography-action-sm-reg flex flex-row gap-1"
+                >
+                  새로운 멤버 초대하기
+                </Button>
+              </div>
+            </div>
+          </div>
+          {/* 여행 시작 박스 */}
+          <div className="p-3 rounded-2xl bg-[#f0f0f0] flex flex-col gap-3">
+            <p className="typography-action-base-bold">
+              이제 여행갈 준비가 되셨나요?
+            </p>
+            <div className="flex flex-col gap-1">
+              <Divider />
+              <Button
+                variant="ghost"
+                className="w-full flex justify-between px-0"
+              >
+                <p className="typography-action-sm-reg">
+                  이 보관함에서 여행 계획 시작하기
+                </p>
+                <ChevronRight size={20} strokeWidth={2} />
+              </Button>
+            </div>
+          </div>
+        </main>
+        <DrawerFooter>
+          <Button variant="ghost">
+            <DoorClosed size={18} className="opacity-40" />
+            <p className="typography-action-sm-reg">이 컬렉션에서 나가기</p>
+          </Button>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default MemberSideDrawer;
