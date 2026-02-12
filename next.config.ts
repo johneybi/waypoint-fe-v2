eed2f3e5d090157946ad1b95015558e0fdf2ee69
Yusi <yusi4781@naver.com>
2026-02-13 00:48:43 +0900

    feat(setting): Image 태그 i.pravatar.cc 도메인 허용 [WAPO-153]

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+    ],
+  },
   turbopack: {
     rules: {
       "*.svg": {
